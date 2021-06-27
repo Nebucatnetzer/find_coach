335e43b07ade55d07e14eebeec498e934748286e
Andreas Zweili <andreas@zweili.ch>
2021-06-27 14:22:20 +0200

    fix a typo

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,19 @@
-import { ceateRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        { path: '/', redirect: '/coaches' },
-        { path: '/coaches', components: null },
-        { path: '/coaches/:id', components: null, children: [
-            { path: 'contact', component: null}
-        ]},
-        { path: '/register', components: null},
-        { path: '/request', components: null},
-        { path: '/notFound(.*)', components: null}
-    ]
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches', components: null },
+    {
+      path: '/coaches/:id',
+      components: null,
+      children: [{ path: 'contact', component: null }]
+    },
+    { path: '/register', components: null },
+    { path: '/request', components: null },
+    { path: '/notFound(.*)', components: null }
+  ]
 });
 
 export default router;
