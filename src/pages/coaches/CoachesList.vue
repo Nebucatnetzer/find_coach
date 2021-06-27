04f1a6a7ec0c92c004aaa03f4b3c85b7ed7afa8f
Andreas Zweili <andreas@zweili.ch>
2021-06-27 17:29:36 +0200

    add basic elements to CoachesList

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -2,5 +2,13 @@
   <section>
     Filter
   </section>
-  <section>List of coaches</section>
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as Coach</router-link>
+    </div>
+    <ul>
+      List of coaches
+    </ul>
+  </section>
 </template>
