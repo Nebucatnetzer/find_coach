f37c15f75550583534479c3100dcc51cdcbd2e50
Andreas Zweili <andreas@2li.ch>
2021-06-28 18:06:58 +0200

    register BaseBadge globally

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue';
 import router from './router';
 import store from './store';
 import App from './App';
+import BaseBadge from './components/ui/BaseBage';
 import BaseButton from './components/ui/BaseButton';
 import BaseCard from './components/ui/BaseCard';
 
@@ -11,6 +12,7 @@ const app = createApp(App);
 app.use(router);
 app.use(store);
 
+app.component('base-badge', BaseBadge);
 app.component('base-button', BaseButton);
 app.component('base-card', BaseCard);
 
