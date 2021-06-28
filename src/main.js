edd1099b002550f6001e3550e61827f0ac243e25
Andreas Zweili <andreas@2li.ch>
2021-06-28 17:58:20 +0200

    register the BaseButton

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createApp } from 'vue';
 import router from './router';
 import store from './store';
 import App from './App';
+import BaseButton from './components/ui/BaseButton';
 import BaseCard from './components/ui/BaseCard';
 
 const app = createApp(App);
@@ -10,6 +11,7 @@ const app = createApp(App);
 app.use(router);
 app.use(store);
 
+app.component('base-button', BaseButton);
 app.component('base-card', BaseCard);
 
 app.mount('#app');
