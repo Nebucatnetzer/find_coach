611e6f5c37dc4da4ac7f85fcbd1ca5f6667f8b9e
Andreas Zweili <andreas@2li.ch>
2021-07-10 16:54:27 +0200

    signal the invalid elements in the form

--- src/components/coaches/CoachForm.vue
+++ src/components/coaches/CoachForm.vue
@@ -1,26 +1,30 @@
 <template>
   <form @submit.prevent="submitForm">
-    <div class="form-control">
+    <div class="form-control" :class="{ invalid: !firstName.isValid }">
       <label for="firstname">Firstname</label>
       <input type="text" id="firstname" v-model.trim="firstName.val" />
+      <p v-if="!firstName.isValid">Firstname must not be empty.</p>
     </div>
-    <div class="form-control">
+    <div class="form-control" :class="{ invalid: !lastName.isValid }">
       <label for="lastname">Lastname</label>
       <input type="text" id="lastname" v-model.trim="lastName.val" />
+      <p v-if="!lastName.isValid">Lastname must not be empty.</p>
     </div>
-    <div class="form-control">
+    <div class="form-control" :class="{ invalid: !description.isValid }">
       <label for="description">Description</label>
       <textarea
         id="description"
         rows="5"
         v-model.trim="description.val"
       ></textarea>
+      <p v-if="!description.isValid">Description must not be empty.</p>
     </div>
-    <div class="form-control">
+    <div class="form-control" :class="{ invalid: !rate.isValid }">
       <label for="rate">Hourly Rate</label>
       <input type="number" id="rate" v-model.number="rate.val" />
+      <p v-if="!description.isValid">Rate must be greater than 0.</p>
     </div>
-    <div class="form-control">
+    <div class="form-control" :class="{ invalid: !areas.isValid }">
       <h3>Areas of Expertise</h3>
       <div>
         <input
@@ -44,6 +48,12 @@
         <input type="checkbox" id="career" value="career" v-model="areas.val" />
         <label for="career">Career Advisory</label>
       </div>
+      <p v-if="!areas.isValid">At least one expertise must be selected.</p>
+    </div>
+    <div v-if="!formIsValid" class="invalid">
+      <p>
+        Please fix the above errors and submit again.
+      </p>
     </div>
     <base-button>Register</base-button>
   </form>
@@ -167,12 +177,7 @@ h3 {
   font-size: 1rem;
 }
 
-.invalid label {
+.invalid p {
   color: red;
-}
-
-.invalid input,
-.invalid textarea {
-  border: 1px solid red;
 }
 </style>
