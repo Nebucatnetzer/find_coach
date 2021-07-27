f3a4c1c686d38d406e9c0a5d498bb6e1881ade34
Andreas Zweili <andreas@zweili.ch>
2021-07-27 11:18:56 +0200

    add Vuex actions for ContactCoach

---
+++ src/store/modules/requests/actions.js
@@ -0,0 +1,11 @@
+export default {
+  ContactCoach(context, payload) {
+    const request = {
+      id: new Date().toISOString(),
+      coachId: payload.coachId,
+      userEmail: payload.email,
+      message: payload.message
+    };
+    context.commit('addRequest', request);
+  }
+};
