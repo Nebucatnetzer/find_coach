c883fcfe9a2fda099532f605b20f7158572b6be7
Andreas Zweili <andreas@zweili.ch>
2021-07-27 11:07:10 +0200

    add validation to ContactForm

--- src/pages/requests/ContactCoach.vue
+++ src/pages/requests/ContactCoach.vue
@@ -1,18 +1,46 @@
 <template>
-  <form>
+  <form @submit.prevent="submitForm">
     <div>
       <label for="email">Your E-Mail</label>
-      <input type="email" id="email" />
+      <input type="email" id="email" v-model.trim="email" />
     </div>
     <div>
       <label for="message">Message</label>
-      <textarea rows="5" id="message"></textarea>
+      <textarea rows="5" id="message" v-model.trim="message"></textarea>
     </div>
+    <p class="errors" v-if="!formIsValid">
+      Please enter a valid email and non-empty message
+    </p>
     <div class="actions">
       <base-button>Send Message</base-button>
     </div>
   </form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      message: '',
+      formIsValid: true
+    };
+  },
+  methods: {
+    submitForm() {
+      this.formIsValid = true;
+      if (
+        this.email === '' ||
+        !this.email.includes('@') ||
+        this.message === ''
+      ) {
+        this.formIsValid = false;
+        return;
+      }
+    }
+  }
+};
+</script>
 
 <style scoped>
 form {
