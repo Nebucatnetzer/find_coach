ff0bdcf93bb085a5d5eb0e2c72e546a1962cd8b9
Andreas Zweili <andreas@zweili.ch>
2021-07-27 11:26:01 +0200

    fix the import for requestsModule

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore } from 'vuex';
 
 import coachesModule from './modules/coaches/index';
-import requestsModule from '.modules/requests/index';
+import requestsModule from './modules/requests/index';
 
 const store = createStore({
   modules: {
