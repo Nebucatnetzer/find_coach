beb804e38c688082b5edf0a9e06400d41af6a258
Andreas Zweili <andreas@2li.ch>
2021-07-28 22:58:02 +0200

    add functions for login

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,7 +1,25 @@
 import getAuth from '../../../scripts/axios-auth';
 
 export default {
-  login() {},
+  async login(context, payload) {
+    try {
+      const response = await getAuth.post('accounts:signInWithPassword', {
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true
+      });
+      const responseData = response.data;
+      console.log(responseData);
+      context.commit('setUser', {
+        token: responseData.idToken,
+        userId: responseData.localId,
+        tokenExpiration: responseData.expiresIn
+      });
+    } catch (err) {
+      const error = new Error(err.message);
+      throw error;
+    }
+  },
   async signup(context, payload) {
     try {
       const response = await getAuth.post('accounts:signUp', {
