4665d05bfa2ea1e0968dcad2f5384df7617c0f32
Andreas Zweili <andreas@2li.ch>
2021-07-28 16:58:14 +0200

    add an action to get coaches from firebase

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -21,6 +21,24 @@ export default {
     } catch (err) {
       console.log(err.response.status);
     }
+  },
+  async loadCoaches(context) {
+    try {
+      const response = await getAPI.get('coaches.json');
+      const responseData = response.data;
+      const coaches = [];
+      for (const key in responseData) {
+        const coach = {
+          id: key,
+          firstName: responseData[key].firstName,
+          lastName: responseData[key].lastName,
+          description: responseData[key].description,
+          hourlyRate: responseData[key].hourlyRate,
+          areas: responseData[key].areas
+        };
+        coaches.push(coach);
+      }
+      context.commit('setCoaches', coaches);
     } catch (err) {
       console.log(err.response.status);
     }
