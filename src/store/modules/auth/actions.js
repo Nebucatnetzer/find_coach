ba84e606c5cbab744a707e1dcb8a61934e9a2237
Andreas Zweili <andreas@2li.ch>
2021-07-28 22:40:49 +0200

    add the signup process

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,3 +1,24 @@
-//import getAPI from '../../../scripts/axios-api';
+import getAuth from '../../../scripts/axios-auth';
 
-export default {};
+export default {
+  login() {},
+  async signup(context, payload) {
+    try {
+      const response = await getAuth.post('accounts:signUp', {
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true
+      });
+      const responseData = response.data;
+      console.log(responseData);
+      context.commit('setUser', {
+        token: responseData.idToken,
+        userId: responseData.localId,
+        tokenExpiration: responseData.expiresIn
+      });
+    } catch (err) {
+      const error = new Error(err.message);
+      throw error;
+    }
+  }
+};
